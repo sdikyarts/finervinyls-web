c66bbeb089b1e9092fef5be1251ce843810f87bd
sdikyarts <--global>
2024-01-28 18:03:20 +0700

    restructurization

--- finervinyls/urls.py
+++ finervinyls/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for finervinyls_web project.
+URL configuration for finervinyls project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
